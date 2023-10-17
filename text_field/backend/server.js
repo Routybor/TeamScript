cbaefe13074e2dba5855cc7845731448772e6ab2
Routybor <routybor@gmail.com>
2023-10-17 20:28:09 +0700

    data synch and loading fix

--- text_field/backend/server.js
+++ text_field/backend/server.js
@@ -1,68 +1,64 @@
-const express = require('express') // To create a server, we will use express
-const app = express()
-const { Client } = require('pg'); // to work with PostgreSQL db, we use pg
-const port = 5000;
-const bodyParser = require('body-parser');  // To get form data from a request
-// , you need to use a special bodyparser package.
+// Ниже перечисляются требуемые модули, если npm ругается на ошибку импорта, сокорее всего это пофиксится 'npm i ' + модуль из этого списка
+const express = require('express') // Фреймворк для js, упрощает работу с HTTP запросами, маршрутизацией и бла бла бла
+const { Client } = require('pg'); // Постгрес, база данных
+const bodyParser = require('body-parser'); // Парсер для запросов с фронтенда (они в формате джсона у нас)
+const socketIo = require('socket.io'); // Для управления сокетами пользователей из сервера. По сути двусторонняя связь между браузером(клиентом) и сервером(теперь запросы отправлять можно и из сервера)
+const http = require('http'); // Модуль для создания HTTP серверов, это всё для связи сервера и клиента (верхний модуль)
 
-// connect to the database
-let text = "text";
-const client = new Client({
+const app = express() // Создание Express приложения, с которым и бкдем работать
+const port = 5000; // Обозначаем порт СЕРВЕРА
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const client = new Client({ // Это база
   user: 'teamscriptproj',
   host: 'ep-rapid-morning-14341499.eu-central-1.aws.neon.tech',
   database: 'Text_input',
   password: 'Sv6q0diQZxfP',
   port: 5432,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Можно навести на первое слово и там написано
   },
 });
 
-client.connect()
+client.connect() // Подключаем БД
   .then(() => {
-    console.log('Connected to PostgreSQL db');
+    console.log('Connected to PostgreSQL db'); // И логируем что всё успешно
   })
+  .catch((error) => {
+    console.error('Error connecting to PostgreSQL db = ', error); // Или не очень
+  });
 
-// start runnig server
-app.listen(port, () => {
-  console.log(`Server is running on port = ${port}`);
-});
-
-/*
-This code is a GET request handler for the '/database' route. 
-It uses the client.query() method to execute a SELECT query on the database. 
-In this case, the query selects the 'text_column' column from the 'text_table' table.
- */
-app.get('/database', (req, res) => {
+app.get('/database', (req, res) => { // GET запрос для получения данных из базы
   client.query('select text_column from text_table', (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Error while getting data from database' });
     } else {
-      res.json(result.rows[0]);
+      res.json(result.rows[0]); // [0] -> первая строка
     }
   });
 });
 
-/*
-This line of code is used to parse incoming JSON data in a Node.js application.
- */
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Подключаем парсер для обработки POST запросов
 
-/**
-This code is a POST request handler for the '/database/saveText' route. 
-It expects an incoming request body with a 'textField' property, which contains the new text value to be saved in the database.
+const sendUpdateToClients = (newText) => { // Функция для отправки в фронтенд
+  io.emit('updateText', { text_column: newText }); //Отправляем сообщение updateText со значением newText с помощью io
+};
 
-The code then uses the client.query() method to execute an SQL UPDATE query on the database. 
-The query updates the 'text_column' column in the 'text_table' table with the new value of newText, but only for the row with 'id' equal to 1.
-
-If the query is executed successfully, the code returns a JSON response with an object containing the new value of newText.
- */
-app.post('/database/saveText', (req, res) => {
-    const newText = req.body.textField;
-    client.query(`UPDATE text_table 
-                  SET text_column = '${newText}'
-                  WHERE id = 1 `, (err, result) => {
+app.post('/database/saveText', (req, res) => { // Запись в базу данных
+  const newText = req.body.textField;
+  client.query(`UPDATE text_table
+                SET text_column = '${newText}'
+                WHERE id = 1 `, (err, result) => {
+    if (!err) {
+      sendUpdateToClients(newText); // После того, как записали изменения одного клиента, отправляем сообщение на фронт всем клиентам
       res.json({ text_column: newText });
-    });
+    }
   });
+});
+
+server.listen(port, () => { // Запуск сервера, вывод в консоль информации для удобства.
+  console.log(`Server is running on port = ${port}`); // +- Бесполезная инфа
+  console.log(`Backend link is = http://localhost:${port}`); // К этой ссылке можно приписать URL и если этот URL используется для GET запроса, это даже будет работать
+});
--- text_field/frontend/src/App.js
+++ text_field/frontend/src/App.js
@@ -1,42 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField } from "@mui/material";
 import './App.css';
+import io from 'socket.io-client';
 
+const socket = io('/'); // С помощью io создаём сокет, когда указываем / метод сам ищет бек, с которого запущен сайт
 
 function App() {
   const [textFieldValue, setTextFieldValue] = useState('');
-  const handleTextFieldChange = (event) => {
-    const newText = event.target.value;
-    fetch('/database/saveText', {
-      method: 'POST',
+  const [isLoading, setIsLoading] = useState(true); // Для обработки загрузок по дефолту загрузка True
+
+  useEffect(() => { // С помощью этого при первой отрисовки сайта данные с  базы данных сразу появляются на странице
+    receiveData(); // Собственно получение данных с БД
+    socket.on('updateText', (data) => { // Начинаем проверять(слушать) сокет на наличие данного сообщения и если оно получено, data из сообщения отправляется далее
+      setTextFieldValue(data.text_column); // Заночим данные в поле
+      setIsLoading(false); // Завершаем получение и объявляем о окончании загрузки
+    });
+    return () => {
+      socket.off('updateText'); // Если страница закрыта во избежание утечек памяти и ненужных процессов завершаем проверять сокет
+    };
+  }, []); // Так как список зависимостей [] пустой => выполнится при загрузке
+
+  const receiveData = () => { // Отправка на бек HTTP запроса и получение ответа(данных с БД)
+    setIsLoading(true); // Пока выполняем объявляем загрузку
+    fetch('/database/', { // URL
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ textField: newText }),
     })
-      .then((response) => { return response.json(); })
-      .then((data) => {
-        setTextFieldValue(data.text_column);
-        console.log('Text saved successfully = ', data);
-      })
-      .catch((error) => {
-        console.error('Error saving text = ', error);
-      });
-      setTextFieldValue(newText);
+    .then((response) => response.json()) // Форматируем ответ в джсон
+    .then((data) => {
+      setTextFieldValue(data.text_column); // Данные в текстовое поле берем из бека
+      setIsLoading(false);
+      console.log('Data received =', data); // Посмотреть = Inspect element -> Console
+    })
+    .catch((error) => {
+      console.error('ERROR receiving data = ', error); // Посмотреть = Inspect element -> Console
+    });
   };
 
+  const updateData = (event) => { // Отправка на бек HTTP запроса и данных для БД и получение ответа(вставленных в БД данных)
+    if (!isLoading) {
+      const newText = event.target.value;
+      setIsLoading(true);
+      fetch('/database/saveText', { // URL
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ textField: newText }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // setTextFieldValue(data.text_column);  // Вообще это уже делает сокет
+          setIsLoading(false);
+          console.log('Text saved = ', data); // Посмотреть = Inspect element -> Console
+        })
+        .catch((error) => {
+          console.error('ERROR saving text = ', error); // Посмотреть = Inspect element -> Console
+        });
+    }
+  };
 
   return (
     <div className="App">
-      <div className="main-content">
-      </div>
       <TextField
         fullWidth
         value={textFieldValue}
         id="standard-basic"
         label="Enter your text here"
         multiline
-        onChange={handleTextFieldChange}/>
+        onChange={updateData}
+      />
     </div>
   );
 }
