871ee14b149b68fcac9a41d8edfe6900c45d2c95
MetlinAlexander <alexmet04@gmail.com>
2023-10-30 00:31:09 +0700

    added the ability to request all the same of this project from DB

--- website/backend/backend.js
+++ website/backend/backend.js
@@ -13,7 +13,7 @@ const io = socketIo(server);
 const client = new Client({ // Это база
   user: 'teamscriptproj',
   host: 'ep-rapid-morning-14341499.eu-central-1.aws.neon.tech',
-  database: 'Text_input',
+  database: 'Tasks',
   password: 'gjBJE57AGHiP',
   port: 5432,
   ssl: {
@@ -28,35 +28,55 @@ client.connect() // Подключаем БД
   .catch((error) => {
     console.error('Error connecting to PostgreSQL db = ', error); // Или не очень
   });
+// закоментил, т.к. это пока не нужно
 
-app.get('/database', (req, res) => { // GET запрос для получения данных из базы
-  client.query('select text_column from text_table', (err, result) => {
+// app.get('/database', (req, res) => { // GET запрос для получения данных из базы
+//   client.query('select text_column from text_table', (err, result) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).json({ error: 'Error while getting data from database' });
+//     } else {
+//       res.json(result.rows[0]); // [0] -> первая строка
+//     }
+//   });
+// });
+
+// app.use(bodyParser.json()); // Подключаем парсер для обработки POST запросов
+
+// const sendUpdateToClients = (newText) => { // Функция для отправки в фронтенд
+//   io.emit('updateText', { text_column: newText }); //Отправляем сообщение updateText со значением newText с помощью io
+// };
+
+// app.post('/database/saveText', (req, res) => { // Запись в базу данных
+//   const newText = req.body.textField;
+//   client.query(`UPDATE text_table
+//                 SET text_column = '${newText}'
+//                 WHERE id = 1 `, (err, result) => {
+//     if (!err) {
+//       sendUpdateToClients(newText); // После того, как записали изменения одного клиента, отправляем сообщение на фронт всем клиентам
+//       res.json({ text_column: newText });
+//     }
+//   });
+// });
+
+
+
+// -------код для работы с тасками---------
+
+// получаем json содержащий все таски текущего проекта
+app.get('/project/tasks', (req, res) => { // GET запрос для получения данных из базы
+  client.query('select * from projecttasks ', (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Error while getting data from database' });
     } else {
-      res.json(result.rows[0]); // [0] -> первая строка
+      res.json(result.rows); // [0] -> первая строка
+      console.log(result.rows);
     }
   });
 });
 
-app.use(bodyParser.json()); // Подключаем парсер для обработки POST запросов
-
-const sendUpdateToClients = (newText) => { // Функция для отправки в фронтенд
-  io.emit('updateText', { text_column: newText }); //Отправляем сообщение updateText со значением newText с помощью io
-};
-
-app.post('/database/saveText', (req, res) => { // Запись в базу данных
-  const newText = req.body.textField;
-  client.query(`UPDATE text_table
-                SET text_column = '${newText}'
-                WHERE id = 1 `, (err, result) => {
-    if (!err) {
-      sendUpdateToClients(newText); // После того, как записали изменения одного клиента, отправляем сообщение на фронт всем клиентам
-      res.json({ text_column: newText });
-    }
-  });
-});
+// ----------------------------------------
 
 server.listen(port, () => { // Запуск сервера, вывод в консоль информации для удобства.
   console.log(`Server is running on port = ${port}`); // +- Бесполезная инфа
