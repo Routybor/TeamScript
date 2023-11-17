6c2fbbf7add19b9651497a5cc8d2042a061abe6c
zelizavetta <e.zaitseva2@g.nsu.ru>
2023-11-17 11:06:13 +0700

    adding localStorage

--- website/frontend/src/App.jsx
+++ website/frontend/src/App.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TransferListComponent from './components/TransferListComponent';
 // import TextFieldComponent from './components/TextFieldComponent';
 import AuthorizationComponent from './components/AuthorizationComponent';
+import useToken from './helper/useToken';
 
 const host = 'http://localhost:5000'
 const socket = io(host);
 
+
 function App() {
+  const { token, setToken } = useToken();
+  if (!token) {
+    return <AuthorizationComponent setToken={setToken} />
+  }
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AuthorizationComponent />} />
+          {/* <Route path="/" element={<AuthorizationComponent setToken={setToken} />} /> */}
           {/* <Route path="/authorization" element={<h1>HI</h1>}>
         </Route> */}
-          <Route path="/taskboard" element={<TransferListComponent socket={socket} host={host} />}>
+          <Route path="/" element={<TransferListComponent socket={socket} host={host} />}>
           </Route>
         </Routes>
       </BrowserRouter >
--- website/frontend/src/components/AuthorizationComponent.jsx
+++ website/frontend/src/components/AuthorizationComponent.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomInputComponent from './CustomInputComponent';
 import "./AuthorizationComponent.css";
 
-const AuthorizationComponent = () => {
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+
+const AuthorizationComponent = ({ setToken }) => {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
 
     return (
         <div className='authorization'>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <CustomInputComponent
-                    labelText="Email"
-                    id="email"
-                    // handleChange={e => setUserName(e.target.value)}
+                    labelText="Login"
+                    id="login"
+                    handleChange={e => setUserName(e.target.value)}
                     type="text"
                 />
                 <CustomInputComponent
                     labelText="Password"
                     id="password"
-                    // handleChange={e => setPassword(e.target.value)}
+                    handleChange={e => setPassword(e.target.value)}
                     type="password"
                 />
                 {/* <Button className='logInButton'>LOG IN</Button> */}
@@ -25,5 +49,6 @@ const AuthorizationComponent = () => {
         </div>
     );
 }
+
 
 export default AuthorizationComponent;
