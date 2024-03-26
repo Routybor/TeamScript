2d2a812834f7f803f2e38a0bd213cdbb7cde76c8
Alexander Metlin <alexmet04@gmail.com>
2024-03-27 00:22:26 +0700

    update init tables

---
+++ website/backend/database/initDataBase.js
@@ -0,0 +1,144 @@
+const pool = require("./dbConnect");
+
+/**
+ * Function to create the users table in the database
+ * 
+ * @param {*} pool - Database connection pool
+ */
+const createUsersTable = (pool) => {
+    pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            mytable_key SERIAL PRIMARY KEY,
+            username VARCHAR(40) NOT NULL,
+            passwd VARCHAR(40) NOT NULL,
+            token TEXT
+        )`,
+        (err, result) => {
+            if (err) {
+                console.error('Error creating users table:', err);
+            } else {
+                console.log('Users table created successfully');
+            }
+        }
+    );
+};
+
+/**
+ * Function to create the user_projects table in the database
+ * 
+ * @param {*} pool - Database connection pool
+ */
+const createUserProjects = (pool) => {
+    pool.query(`
+        CREATE TABLE IF NOT EXISTS user_projects (
+            project_id  integer,
+            user_id integer
+        )`,
+        (err, result) => {
+            if (err) {
+                console.error('Error creating UserProjects table:', err);
+            } else {
+                console.log('UserProjects table created successfully');
+            }
+        }
+    );
+};
+
+/**
+ * Function to create the projects table in the database
+ * 
+ * @param {*} pool - Database connection pool
+ */
+const createProjectsTable = (pool) => {
+    pool.query(`
+        CREATE TABLE IF NOT EXISTS projects (
+            project_id  integer,
+            project_name varchar(40),
+            creator varchar(40),
+            date date
+        )`,
+        (err, result) => {
+            if (err) {
+                console.error('Error creating Projects table:', err);
+            } else {
+                console.log('Projects table created successfully');
+            }
+        }
+    );
+};
+
+/**
+ * Function to create the project_state table in the database
+ * 
+ * @param {*} pool - Database connection pool
+ */ 
+const createProjectsStateTable = (pool) => {
+    pool.query(`
+        CREATE TABLE IF NOT EXISTS project_state (
+            project_id  integer,
+            row_state varchar(40)
+        )`,
+        (err, result) => {
+            if (err) {
+                console.error('Error creating project_state table:', err);
+            } else {
+                console.log('project_state table created successfully');
+            }
+        }
+    );
+};
+
+/**
+ * Function to check if a table exists
+ * 
+ * @param {string} tableName - Name of the table to check
+ * @returns {Promise<boolean>} - Promise resolving to true if the table exists, false otherwise
+ */
+async function tableExists(tableName) {
+    try {
+        const result = await pool.query(
+            `SELECT EXISTS (
+                SELECT 1
+                FROM information_schema.tables 
+                WHERE table_name = $1
+            );`,
+            [tableName]
+        );
+        return result.rows[0].exists;
+    } catch (error) {
+        console.error('Error checking the existence of the table:', error);
+        throw error;
+    }
+}
+
+const tables = ['users', 'user_projects', 'projects', 'project_state'];
+const functionsList = [createUsersTable, createUserProjects, createProjectsTable, createProjectsStateTable];
+
+/**
+ * Function to check the existence of tables and create them if they do not exist
+ */
+async function checkTablesExistence() {
+    try {
+        for (const [index, tableName] of tables.entries()) {
+            if (!(await tableExists(tableName))) {
+                if (functionsList[index]) {
+                    console.log(`Table ${tableName} not exist. Calling function ${functionsList[index].name}...`);
+                    await functionsList[index](pool); // Call function from functionsList
+                } else {
+                    console.log(`Table ${tableName} not exist and no corresponding function found.`);
+                }
+            } else {
+                console.log(`Table ${tableName} exist.`);
+            }
+        }
+    } catch (err) {
+        console.log('Error checking tables:', err);
+    }
+}
+
+module.exports = {
+    checkTablesExistence
+};
+
+// Call the function to check tables existence
+checkTablesExistence();
--- website/backend/server.js
+++ website/backend/server.js
@@ -4,20 +4,26 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const RateLimit = require('express-rate-limit');
+
+// Rate limiter to prevent abuse
 const limiter = RateLimit({
-    windowMs: 60 * 1000,
-    max: 50000,
+    windowMs: 60 * 1000, // 1 minute window
+    max: 50000, // limit each IP to 50000 requests per windowMs
 });
 const app = express();
 const port = 5000;
 const server = http.createServer(app);
+
+// Socket.io for real-time communication
 const socket = require("./socket");
 socket.configureSocket(server);
+
+// Middleware setup
 app.use(cors());
 app.use(limiter);
 app.use(bodyParser.json());
 
-// --------------------------------------------------------------------------------------------------
+// Router setup
 const projectRouter = require("./router/projectRouter");
 const taskRouter = require("./router/taskRouter");
 const authRouter = require("./router/authRouter");
@@ -26,11 +32,16 @@ const rowRouter = require("./router/rowRouter");
 // const textRouter = require("./router/textRouter");
 // app.use('/text', textRouter);
 
+// Mount routers
 app.use('/project', projectRouter)
 app.use('/taskboard', taskRouter);
 app.use('/auth', authRouter)
 app.use('/row', rowRouter)
 
+// Initialize database tables
+const checkTablesExistence = require("./database/initDataBase");
+
+// Start the server
 server.listen(port, () => {
     console.log(`Backend link is = http://localhost:${port}`);
 });
